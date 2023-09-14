2c8373a17d6d896b358607d13362e483a751e627
Ankit NL <ankit.jha@nineleaps.com>
2023-09-14 10:16:20 +0530

    Bhashini POC

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import './App.css';
-import Translation from './Translation'; // Import the Translation component
 import ASR from './ASR';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <div className="App">
       <main>
         <ASR/>
-        {/* <Translation /> Use the Translation component */}
       </main>
     </div>
   );
